93b486c57e27280877b7c6aa05c9a1fb35646477
yg-PC\yg <persnet3@naver.com>
2021-12-18 20:14:22 +0900

    MQTT Demo 프로젝트 + ioc 파일 별도로 만들어서 code gen 기능 함께 사용할 수 있도록 병합

---
+++ Proj_ForHwCfgCodeGen/Core/Inc/main.h
@@ -0,0 +1,101 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+extern RNG_HandleTypeDef hrng;  /* Used by the mbedtls entropy function */
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define USER_BUTTON_Pin GPIO_PIN_13
+#define USER_BUTTON_GPIO_Port GPIOC
+#define USER_BUTTON_EXTI_IRQn EXTI15_10_IRQn
+#define ES_WIFI_RESET_Pin GPIO_PIN_8
+#define ES_WIFI_RESET_GPIO_Port GPIOE
+#define I2C2_SCL_Pin GPIO_PIN_10
+#define I2C2_SCL_GPIO_Port GPIOB
+#define I2C2_SDA_Pin GPIO_PIN_11
+#define I2C2_SDA_GPIO_Port GPIOB
+#define ES_WIFI_BOOT0_Pin GPIO_PIN_12
+#define ES_WIFI_BOOT0_GPIO_Port GPIOB
+#define ES_WIFI_WAKE_UP_Pin GPIO_PIN_13
+#define ES_WIFI_WAKE_UP_GPIO_Port GPIOB
+#define USER_LED2_Pin GPIO_PIN_14
+#define USER_LED2_GPIO_Port GPIOB
+#define SPBTLE_RF_SPI3_CSN_Pin GPIO_PIN_13
+#define SPBTLE_RF_SPI3_CSN_GPIO_Port GPIOD
+#define SPI3_SCK_Pin GPIO_PIN_10
+#define SPI3_SCK_GPIO_Port GPIOC
+#define SPI3_MISO_Pin GPIO_PIN_11
+#define SPI3_MISO_GPIO_Port GPIOC
+#define SPI3_MOSI_Pin GPIO_PIN_12
+#define SPI3_MOSI_GPIO_Port GPIOC
+#define ES_WIFI_NSS_Pin GPIO_PIN_0
+#define ES_WIFI_NSS_GPIO_Port GPIOE
+#define ES_WIFI_DATA_READY_Pin GPIO_PIN_1
+#define ES_WIFI_DATA_READY_GPIO_Port GPIOE
+#define ES_WIFI_DATA_READY_EXTI_IRQn EXTI1_IRQn
+void   MX_SPI3_Init(void);
+void   MX_IWDG_Init(void);
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
